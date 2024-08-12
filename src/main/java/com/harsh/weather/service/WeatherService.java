2d9702c6ce66a69df45ca296264b039805d52c4b
harshyadav02 <harshyadav5736@gmail.com>
2024-08-12 19:25:59 +0530

    logic to get weather data based on city

---
+++ src/main/java/com/harsh/weather/service/WeatherService.java
@@ -0,0 +1,71 @@
+package com.harsh.weather.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class WeatherService {
+    @Value("${weather.api.uri}")
+    private String apiUrl;
+
+    @Value("${weather.api.key}")
+    private String apiKey;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public Map<String, Object> getWeatherData(String city) {
+        // map too store final data
+        Map<String, Object> weatherData = new HashMap<>();
+        try {
+
+            // seting up url of weather data
+            String url = String.format("%s?q=%s&appid=%s", apiUrl, city, apiKey);
+
+            // retirving data from api
+            
+            Map<String, Object> response = restTemplate.getForObject(url, Map.class);
+            // System.out.println(response);
+            // creating map from the retrived data
+            if (response != null) {
+                
+                Map<String, Object> main = (Map<String, Object>) response.get("main");
+                
+                Map<String, Object> wind = (Map<String, Object>) response.get("wind");
+
+                // adding the data to final map
+                weatherData.put("temp_max", main.get("temp_max"));
+                weatherData.put("temp_min", main.get("temp_min"));
+                weatherData.put("humidity", main.get("humidity"));
+                weatherData.put("pressure", main.get("pressure"));
+                weatherData.put("tempCel", main.get("temp"));
+                weatherData.put("tempFah", (Double) main.get("temp") * 9 / 5 + 32);
+
+                weatherData.put("wind", wind.get("deg"));
+                weatherData.put("location", response.get("name"));
+
+                
+                
+                List<Map<String,Object>> weatherList = (List<Map<String,Object>>)(response.get("weather"));
+                weatherData.put("Desc",weatherList.get(0).get("description"));
+                weatherData.put("icon",weatherList.get(0).get("icon"));
+                // System.out.println(weatherList.get(0).);
+                System.out.println(weatherList);
+                return weatherData;
+            }
+            else{
+                return Map.of("error","No data found for city"+city);
+            }
+
+        } catch (Exception e) {
+
+            return Map.of("error","something went wrong");
+        }
+    }
+}
